55acb322c996f4761b0d126b4ae4cfb2b82853df
Yuzukii <benjugarcia01@gmail.com>
2026-09-03 14:23:14 +0800

    chore: remove temp script

--- fix_readme.ps1
+++
@@ -1,7 +0,0 @@
-$bytes = [System.IO.File]::ReadAllBytes("c:\Users\ADMiN\OneDrive\Desktop\Cova Vault\cova-vault\README.md")
-$text = [System.Text.Encoding]::UTF8.GetString($bytes)
-$replaced = $text -replace ([char]0xFFFD), '-'
-$newBytes = [System.Text.Encoding]::UTF8.GetBytes($replaced)
-[System.IO.File]::WriteAllBytes("c:\Users\ADMiN\OneDrive\Desktop\Cova Vault\cova-vault\README.md", $newBytes)
-Write-Output "Done. Tail of file:"
-Get-Content "c:\Users\ADMiN\OneDrive\Desktop\Cova Vault\cova-vault\README.md" -Tail 5
